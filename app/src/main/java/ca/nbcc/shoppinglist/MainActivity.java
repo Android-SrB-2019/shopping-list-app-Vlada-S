a916e9d73cc2f5db4077edd3a45ca42d9af25f6e
Vlada Shevtsova <vladusyafire@gmail.com>
2019-02-10 00:09:14 -0400

    Assignment1 finished. If you get "Cannot resolve symbol 'R'", it seems like it is a bug of Android studio. It works fine if you launch it. I tried to google this error and fix it, but I didn't find a solution that works (all of them asked me to fix errors in xml files and I don't have any errors there).

--- app/src/main/java/ca/nbcc/shoppinglist/MainActivity.java
+++ app/src/main/java/ca/nbcc/shoppinglist/MainActivity.java
@@ -1,13 +1,60 @@
 package ca.nbcc.shoppinglist;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String LOG_TAG =
+            MainActivity.class.getSimpleName();
+    public static final int TEXT_REQUEST = 1;
+
+    private ShoppingList shoppingList = new ShoppingList();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void launchSecondActivity(View view) {
+        Log.d(LOG_TAG, "Button clicked!");
+        //launch second activity
+        Intent intent = new Intent(this, SecondActivity.class);
+        startActivityForResult(intent, TEXT_REQUEST);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode,
+                                 int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TEXT_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                String item = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+
+                shoppingList.addItem(item);
+                showList();
+            }
+        }
+    }
+
+    private void showList(){
+        LinearLayout ll = findViewById(R.id.item_layout);
+        ll.removeAllViews();
+        List<ShoppingList.Item> newList = shoppingList.getList();
+        for(ShoppingList.Item item : newList){
+            TextView tv = new TextView(this);
+            tv.setText(item.name + " " + item.count.toString());
+            tv.setTextSize( 30 );
+            ll.addView(tv);
+        }
     }
 }
